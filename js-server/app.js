dd07697bd5d0856b80109750a03b422e3209aca3
Gerson Ruiz <geros9912@gmail.com>
2024-09-17 00:51:56 -0600

    controller de usuario y autenticacion

--- js-server/app.js
+++ js-server/app.js
@@ -8,13 +8,14 @@ const port = 3000;
 const db = require('./conexion/conexion');
 //Rutas de usuarios
 const usersRoutes = require('./routes/user_routes');
+const authRoutes = require('./routes/auth_routes');
 
 app.use(express.json());
 app.use(morgan('dev'));
 
 
 //Usar las rutas
-app.use('/', usersRoutes);
+app.use('/', usersRoutes,authRoutes);
 
 
 app.listen(port, () =>{
--- js-server/routes/user_routes.js
+++ js-server/routes/user_routes.js
@@ -4,10 +4,17 @@ const rutasUsuario =  require('../controllers/user_controller')
 const multer = require('multer');
 const upload = multer({ storage: multer.memoryStorage() }); // Configuración para manejar archivos
 
-router.get('/users',(req,res) =>{
-    res.send('users todos')
-});
 
+
+//mostrar usuarios
+router.get('/users',rutasUsuario.get_users)
+//mostrar usuario por su id
+router.get('/users/:user_id',rutasUsuario.get_user)
+//registrar usuario
 router.post('/users', upload.single('image'), rutasUsuario.addUser);
+//actualizar usuario
+router.patch('/users/:user_id',upload.single('image'),rutasUsuario.update_user);
+//eliminar usuario
+router.delete('/users/:user_id', rutasUsuario.delete_user_route);
 
 module.exports = router;
